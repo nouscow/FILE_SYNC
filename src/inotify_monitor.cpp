d18799d45587056e699b80a3ca1c368e8865f490
nouscow <144879603@qq.com>
2026-07-08 21:48:10 +0800

    add poll

--- src/inotify_monitor.cpp
+++ src/inotify_monitor.cpp
@@ -6,12 +6,13 @@
 #ifdef __linux__
 #include "inotify_monitor.h"
 #include "monitor.h"
+#include"logger.h"
 #include <thread>
 #include <string>
 #include <iostream>
 #include <sys/inotify.h>
 #include <unistd.h>
-
+#include<poll.h>
 // 构造：保存监视目录路径，传递回调给基类
 inotify_monitor::inotify_monitor(std::string watch_dir,SyncCallback callback):watch_dir(watch_dir),Monitor(std::move(callback)){
 }
@@ -49,13 +50,30 @@ void inotify_monitor::stop(){
 // 主循环：阻塞读取 inotify 事件，解析 inotify_event 结构体，
 // 仅对 IN_CREATE / IN_MODIFY / IN_DELETE / IN_MOVED_TO 事件触发 callback
 void inotify_monitor::loop() {
+    char buf[4096];
+    struct pollfd pfd;
+    pfd.fd=this->_fd_;
+    pfd.events=POLLIN;
     while (running_) {
-        char buf[4096];
-        struct inotify_event*event;
-        int event_len=sizeof(struct inotify_event);
-        int rd=read(this->_fd_,buf,sizeof(buf));
-        int pos=0;
-        while(rd>pos){
+        int ret=poll(&pfd,1,500);
+        if(ret<0){
+            Logger::get().error("poll error"+__LINE__);
+
+            if(errno==EINTR)continue;
+
+            break;
+        }
+        if(ret==0){
+            Logger::get().debug("time out");
+            continue;
+        }
+        if(pfd.revents&POLLIN){
+             int rd=read(this->_fd_,buf,sizeof(buf));
+              struct inotify_event*event;
+             int event_len=sizeof(struct inotify_event);
+      
+          int pos=0;
+           while(rd>pos){
             event=(struct inotify_event*)(buf+pos);
             if(event->len > 0 && (event->mask & (IN_CREATE | IN_MODIFY | IN_DELETE | IN_MOVED_TO))){
                 if (running_ && callback) {
@@ -64,7 +82,10 @@ void inotify_monitor::loop() {
             }
             int upsize=event_len+event->len;
             pos+=upsize;
+            }
         }
+       
+       
     }
 }
 #endif
